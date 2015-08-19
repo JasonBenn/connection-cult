5c8bdbd6a7a6fbbb28bc1733dd3b56ba38c9dbba
Jake R Huhn <imasidonion@gmail.com>
2015-08-18 17:57:37 -0700

    adds api method

--- app.rb
+++ app.rb
@@ -4,3 +4,7 @@ require "sinatra/reloader" if development?
 get '/' do
   send_file './views/index.html'
 end
+
+post 'email_data' do 
+  mailchimp = Mailchimp::API.new(a4e7beab721629d17fdd72321c70cd11-us7)
+end 
